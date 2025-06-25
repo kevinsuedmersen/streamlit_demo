979af18bad09887c050ccf26e6b43d35906e160b
kevinsuedmersen <kevinsuedmersen@gmail.com>
2025-06-25 18:04:05 +0200

    mocking server response

--- src/error_handling.py
+++ src/error_handling.py
@@ -1,39 +1,12 @@
 """Error handling decorators."""
 import logging
 import traceback
-from typing import Callable
+import requests
 
-import requests.exceptions
-import streamlit as st
+from typing import Callable
+from unittest.mock import Mock
 
 logger = logging.getLogger(__name__)
-
-
-def handle_any_exception(func: Callable) -> Callable:
-    """Decorator handling any possible exception. Logging complete error message to console and displaying minimum
-    error info to the user.
-
-    Parameters
-    ----------
-    func: Callable
-        Function to decorate.
-
-    Returns
-    -------
-    Callable
-    """
-    def _wrapper(*args, **kwargs) -> None:
-        try:
-            return func(*args, **kwargs)
-        except Exception as error:
-            logger.error(f"The following unexpected error occurred: {repr(error)}")
-            traceback.print_exc()
-            st.markdown(
-                f"The following error occurred while processing your request:\n`{repr(error)}`.\n\n"
-                "Please contact an administrator and show him/her this error messsage."
-            )
-
-    return _wrapper
 
 
 def handle_invalid_request(request_func: Callable) -> Callable:
@@ -53,8 +26,15 @@ def handle_invalid_request(request_func: Callable) -> Callable:
             response = request_func(*args, **kwargs)
             response.raise_for_status()
             return response
-        except requests.exceptions.HTTPError as err:
-            logger.error(f"HTTP error occurred: {err}, Status code: {err.response.status_code}, Reason: {err.response.reason}")
-            raise err
+        except Exception as err:
+            # Mock an internal server error
+            logger.error(f"The following error occurred while processing a request: {err}")
+            traceback.print_exc()
+            mock_response = Mock()
+            mock_response.status_code = 500
+            mock_response.text = "Internal Server Error"
+            mock_response.json.return_value = {"error": "Internal Server Error"}
+            mock_response.ok = False
+            return mock_response
 
     return _wrapper
--- src/pages/_hello_world_page.py
+++ src/pages/_hello_world_page.py
@@ -3,7 +3,6 @@ import streamlit as st
 from src.pages._base_page import BasePage
 from src.settings import Settings
 from src.clients import BackendAPIClient
-from src.error_handling import handle_any_exception
 
 
 class HelloWorldPage(BasePage):
@@ -11,16 +10,15 @@ class HelloWorldPage(BasePage):
         super().__init__(settings)
         self._backend_api_client = backend_api_client
 
-    @handle_any_exception
     def display(self) -> None:
         if st.session_state.is_successful_connection is None:
             st.write("Connection to the backend hasn't been tested yet.")
 
-            if st.button("Test Connection"):
-                # Test connection and cache the result
-                st.session_state.is_successful_connection = self._backend_api_client.test_connection()
+        if st.button("Test Connection"):
+            # Test connection and cache the result
+            st.session_state.is_successful_connection = self._backend_api_client.test_connection()
 
-                if st.session_state.is_successful_connection:
-                    st.write(f"Connecting to the backend api worked!")
-                else:
-                    st.error("Connecting to the backend api failed.")
+        if (st.session_state.is_successful_connection is not None) and (st.session_state.is_successful_connection == True):
+            st.write(f"Connecting to the backend api worked!")
+        elif (st.session_state.is_successful_connection is not None) and (st.session_state.is_successful_connection == False):
+            st.error("Connecting to the backend api failed.")
